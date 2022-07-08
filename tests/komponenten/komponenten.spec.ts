24e6e2ebb3d51c1395f33a79400acef70c3eb163
SPECSTET <specstet@gmail.com>
2022-07-08 10:48:23 +0200

    New Baseline

--- tests/komponenten/komponenten.spec.ts
+++ tests/komponenten/komponenten.spec.ts
@@ -29,8 +29,29 @@ test.describe(process.env.SLUIE2ET_ENV.toUpperCase() + ' - Komponententests @com
   test(record.title, async ({ page, browser }) => {
     const browserContext = await browser.newContext();
     browserContext.clearCookies();
+    
+    // const imgResponse = await page.request.fetch('https://picsum.photos/id/1000/2000/2000?grayscale');
+    // await page.route('**/*', async route => {
+    //   if(route.request().resourceType() === 'image' && route.request().url().endsWith('.jpg')){
+    //     route.fulfill({
+    //       response: imgResponse
+    //     });        
+    //   } else {
+    //     route.continue();
+    //   }
+    // });
+
     await page.goto(process.env.SLUIE2ET_URL + record.url);
-    await page.waitForLoadState('networkidle');
+    await page.reload({ waitUntil: 'domcontentloaded' });
+    await page.waitForTimeout(2000);
+    
+    const handle = await page.$('header');
+    if(handle){
+      await handle.waitForElementState('stable');
+    }
+    
+    await page.waitForTimeout(3000);
+
     await page.evaluate(async () => {
       const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
       for (let i = 0; i < document.body.scrollHeight; i += 10) {
@@ -41,23 +62,13 @@ test.describe(process.env.SLUIE2ET_ENV.toUpperCase() + ' - Komponententests @com
     });
     await page.waitForLoadState('networkidle');
 
-    // https://addyosmani.com/blog/puppeteer-recipes/#request-interception
-    // https://developers.apify.com/academy/puppeteer-playwright/reading-intercepting-requests#modifyng-the-request
-    // https://picsum.photos/id/870/200/300?grayscale&blur=2
-    await page.evaluate(async () => {
-      let mImages = document.getElementsByClassName('m-mood-image__image');
-      if(mImages.length > 0){
-        document.getElementsByClassName('m-mood-image__image')[0].style.visibility = 'hidden';
-      }
-    });
-
     await page.waitForTimeout(2000);
 
     expect(
       await page.screenshot({ 
         animations: 'disabled', 
         mask: [
-          //page.locator('.m-mood-image__image')
+          
         ],
         caret: 'hide',
         fullPage: true
@@ -68,6 +79,5 @@ test.describe(process.env.SLUIE2ET_ENV.toUpperCase() + ' - Komponententests @com
       });
   });
 }
-  
 
 });
