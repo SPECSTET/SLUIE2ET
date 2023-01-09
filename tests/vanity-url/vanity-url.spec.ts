8cd3e192eba38442cf3dcf65c3439bea17c325db
Andreas Karz <andreas.karz@outlook.com>
2023-01-09 15:41:59 +0100

    ...

--- tests/vanity-url/vanity-url.spec.ts
+++ tests/vanity-url/vanity-url.spec.ts
@@ -4,52 +4,40 @@ import { parse } from 'csv-parse/sync';
 
 // Deactivate Screenshots and Video recording for vanity url tests
 test.use({
-  screenshot: 'off',
-  video: 'off'
+	screenshot: 'off',
+	video: 'off',
 });
 
 // Load the list with Vanity URLs
-const records = parse(
-  fs.readFileSync('./tests/vanity-url/vanity-url.csv'),
-  {
-    columns: true,
-    skip_empty_lines: true,
-    bom: true,
-    delimiter: ';',
-  }
-);
+const records = parse(fs.readFileSync('./tests/vanity-url/vanity-url.csv'), {
+	columns: true,
+	skip_empty_lines: true,
+	bom: true,
+	delimiter: ';',
+});
 
 test.describe('Validate Vanity URLs @vanitys', () => {
+	for (const record of records) {
+		test(`Vanity URL: ${record.vanity}`, async ({ page }) => {
+			const [_, response] = await Promise.all([
+				page.goto(process.env.SLUIE2ET_URL + `/${record.vanity}`, {
+					waitUntil: 'networkidle',
+				}),
+				page.waitForEvent(
+					'response',
+					(response) => response.request().resourceType() === 'document'
+				),
+			]);
 
-  // Reset generated validation CSV file
-  // fs.writeFileSync('vanity-url-validation.csv', 'vanity;url;status\n');
+			var url = page.url();
 
-  for (const record of records) {
-    test(`Vanity URL: ${record.vanity}`, async ({ page }) => {
-      const [_, response] = await Promise.all([
-        page.goto(process.env.SLUIE2ET_URL + `/${record.vanity}`, {
-          waitUntil: 'networkidle',
-        }),
-        page.waitForEvent(
-          'response',
-          (response) => response.request().resourceType() === 'document'
-        ),
-      ]);
+			// Result output for validation
+			fs.appendFileSync(
+				'vanity-url-validation.csv',
+				`${record.vanity};${url};${response.status()}\n`
+			);
 
-      var url = page.url();
-
-      // Result output for validation
-      fs.appendFileSync(
-        'vanity-url-validation.csv',
-        `${record.vanity};${url};${response.status()}\n`
-      );
-      
-      expect(response.status()).not.toBe(404);
-
-      // expect(
-      //   url,
-      //   `Vanity '${record.vanity}' sollte '${record.url}' sein, ist aber ${url}`
-      // ).toContain(record.url);
-    });
-  }
+			expect(response.status()).not.toBe(404);
+		});
+	}
 });
